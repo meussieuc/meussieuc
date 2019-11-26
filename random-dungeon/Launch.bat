8bf40c645205fcc505ef07264277a460a77ca5a4
PEPIN Mathieu <ext-mpepin@eramet-sln.nc>
2019-11-26 16:55:53 +1100

    feat(random-dungeon): add look selection

--- random-dungeon/Launch.bat
+++ random-dungeon/Launch.bat
@@ -1,6 +1,6 @@
 @echo off
-C:
-cd "C:\Program Files\Google\Chrome\Application"
-set "file=test.htm"
+REM C:
+REM cd "C:\Program Files\Google\Chrome\Application"
+set "file=index.html"
 set "path=%~dp0%file%"
 start chrome.exe --allow-file-access-from-files -incognito %path%
